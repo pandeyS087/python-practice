8db84b3ab580ce1271a4a89b8ffb4d6d1d18c92d
Shriya Pandey <pandeyshriya.087@gmail.com>
2026-03-08 16:25:50 +0530

    Added count vowels program

---
+++ strings/count_vowels.py
@@ -0,0 +1,15 @@
+s = input("Enter a string: ")
+
+vowels = "aeiouAEIOU"
+v = 0
+c = 0
+
+for ch in s:
+    if ch.isalpha():
+        if ch in vowels:
+            v += 1
+        else:
+            c += 1
+
+print("Vowels:", v)
+print("Consonants:", c)
